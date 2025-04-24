758355ed2f9a557947a4bfdc6eb26fdc12576101
Rishit Tiwari <irishittiwari@gmail.com>
2025-04-24 13:42:20 +0530

    Fix Supabase auth

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -6,7 +6,7 @@ export async function GET(request: Request) {
   const code = searchParams.get('code');
 
   if (code) {
-    const supabase = createServerClient();
+    const supabase = await createServerClient();
     await supabase.auth.exchangeCodeForSession(code);
   }
 
